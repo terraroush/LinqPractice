7a51381470bd1c94be687d6cca9688a872ad2c62
Terra Roush <terra.roush@gmail.com>
2020-11-19 16:06:37 -0600

    lists all numbers from the original list and stops before the first perfect square

--- Program.cs
+++ Program.cs
@@ -92,10 +92,50 @@ namespace linq
 
             Console.WriteLine(prices.Max());
             Console.WriteLine("");
+            Console.WriteLine("Partitioning Operations");
+            Console.WriteLine("");
+
+            // Store each number in the following List until a perfect square
+            // is detected.
+
+            // Expected output is { 66, 12, 8, 27, 82, 34, 7, 50, 19, 46 } 
+            List<int> wheresSquaredo = new List<int>()
+{
+    66, 12, 8, 27, 82, 34, 7, 50, 19, 46, 81, 23, 30, 4, 68, 14
+};
+
+            List<int> noPerfectSquare = wheresSquaredo.TakeWhile(num =>
+            {
+                return Math.Sqrt(num) % 1 != 0;
+            }).ToList();
+
+            noPerfectSquare.ToList().ForEach(num => Console.WriteLine(num));
+            Console.WriteLine("");
 
 
+            // // Build a collection of customers who are millionaires
+            //         public class Customer
+            //         {
+            //             public string Name { get; set; }
+            //             public double Balance { get; set; }
+            //             public string Bank { get; set; }
+            //         }
 
+
+            //         List<Customer> customers = new List<Customer>() {
+            //             new Customer(){ Name="Bob Lesman", Balance=80345.66, Bank="FTB"},
+            //             new Customer(){ Name="Joe Landy", Balance=9284756.21, Bank="WF"},
+            //             new Customer(){ Name="Meg Ford", Balance=487233.01, Bank="BOA"},
+            //             new Customer(){ Name="Peg Vale", Balance=7001449.92, Bank="BOA"},
+            //             new Customer(){ Name="Mike Johnson", Balance=790872.12, Bank="WF"},
+            //             new Customer(){ Name="Les Paul", Balance=8374892.54, Bank="WF"},
+            //             new Customer(){ Name="Sid Crosby", Balance=957436.39, Bank="FTB"},
+            //             new Customer(){ Name="Sarah Ng", Balance=56562389.85, Bank="FTB"},
+            //             new Customer(){ Name="Tina Fey", Balance=1000000.00, Bank="CITI"},
+            //             new Customer(){ Name="Sid Brown", Balance=49582.68, Bank="CITI"}
+            //         };
 
         }
+
     }
 }
