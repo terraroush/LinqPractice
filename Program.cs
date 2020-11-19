07ee07568b888c1f5179fa960bb5299426425dd1
Terra Roush <terra.roush@gmail.com>
2020-11-19 14:07:42 -0600

    used Sum to total amounts in purchase list

--- Program.cs
+++ Program.cs
@@ -81,6 +81,8 @@ namespace linq
 {
     2340.29, 745.31, 21.76, 34.03, 4786.45, 879.45, 9442.85, 2454.63, 45.65
 };
+            Console.WriteLine(purchases.Sum());
+            Console.WriteLine("");
 
 
 
